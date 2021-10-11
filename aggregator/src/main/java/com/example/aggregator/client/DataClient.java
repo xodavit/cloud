6941d6ed8d89fb3f2f05eed7bc5a1df29c9bbab8
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-10-11 15:21:53 +0300

    add aggregator logic

--- aggregator/src/main/java/com/example/aggregator/client/DataClient.java
+++ aggregator/src/main/java/com/example/aggregator/client/DataClient.java
@@ -1,11 +1,18 @@
 package com.example.aggregator.client;
 
+import com.example.aggregator.dto.Payment;
 import com.example.aggregator.dto.ResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @FeignClient(value = "data")
 public interface DataClient {
   @GetMapping
   ResponseDto getValue();
+
+  @GetMapping("/payments/{count}")
+  List<Payment> getList(@PathVariable int count);
 }
