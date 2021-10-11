55c5392bbc5e89eefd066f902b94a3d61346cff5
vitaliy_khodataev <khodataev.v@yandex.ru>
2021-10-11 17:32:24 +0300

    fix kafka service

--- consumer/src/main/java/com/example/consumer/service/KafkaService.java
+++ consumer/src/main/java/com/example/consumer/service/KafkaService.java
@@ -4,7 +4,9 @@ import com.example.consumer.data.Payment;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
@@ -21,28 +23,13 @@ public class KafkaService {
     private final String KEY = "consumer.payment";
     private final KafkaTemplate<String, Payment> template;
 
-//    @org.springframework.kafka.annotation.KafkaListener(groupId = "consumer", topics = "payments")
-//    public void listen(Payment message, ConsumerRecord<String, Payment> record, Acknowledgment acknowledgment) {
-//        log.info(String.valueOf(message));
-//        acknowledgment.acknowledge();
-//    }
-
-    /**
-     * Collect messages from topic "payments" and send to other topic
-     *
-     * @param message current message from topic
-     */
-    @org.springframework.kafka.annotation.KafkaListener(groupId = CONSUMER, topics = TOPIC_PAYMENTS)
-    public void listen(Payment message) {
+    @KafkaListener(groupId = CONSUMER, topics = TOPIC_PAYMENTS)
+    public void listen(Payment message, Acknowledgment acknowledgment) {
         log.info(String.valueOf(message));
         send(message);
+        acknowledgment.acknowledge();
     }
 
-    /**
-     * Send the payment to topic "other.payments"
-     *
-     * @param payment which we should to send
-     */
     public void send(Payment payment) {
         final ListenableFuture<SendResult<String, Payment>> future = template.send(
                 new ProducerRecord<>(TOTOPIC, KEY, payment));
--- data/src/main/java/com/example/data/service/KafkaService.java
+++ data/src/main/java/com/example/data/service/KafkaService.java
@@ -5,18 +5,24 @@ import com.example.data.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
 @Log
 public class KafkaService {
     private final PaymentRepository repository;
+    private final String TOPIC_PAYMENTS = "other.payments";
+    private final String CONSUMER = "data.consumers";
 
-    @KafkaListener(groupId = "data.consumers", topics = "other.payments")
-    public void listen(Payment message) {
+
+    @KafkaListener(groupId = CONSUMER, topics = TOPIC_PAYMENTS)
+    public void listen(Payment message, Acknowledgment acknowledgment) {
         log.info(String.valueOf(message));
-//        acknowledgment.acknowledge();
         repository.save(message);
+        acknowledgment.acknowledge();
     }
+
 }
